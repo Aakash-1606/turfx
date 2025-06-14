3a783a62c707124d1ccdb6b5add7375c04559416
Aakash-1606 <146339318+Aakash-1606@users.noreply.github.com>
2025-06-14 17:39:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/', // 👈 ✅ THIS is the fix for white screen on custom domain
   server: {
     host: "::",
     port: 8080,
